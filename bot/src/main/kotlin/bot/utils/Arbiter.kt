81b3af7d6f524b782a7823c72492035bb78a9a15
Red Scott <damngates@yahoo.com>
2019-09-18 12:40:16 -0700

    added debugging line to verify init is called

--- bot/src/main/kotlin/bot/utils/Arbiter.kt
+++ bot/src/main/kotlin/bot/utils/Arbiter.kt
@@ -16,6 +16,7 @@ import java.util.concurrent.TimeUnit
 
 object Arbiter {
     init {
+        println("XXX calling init")
         setupTransactionsBridge()
         setupScoreUpdateBridge()
         setupCloseScoreUpdateBridge()
