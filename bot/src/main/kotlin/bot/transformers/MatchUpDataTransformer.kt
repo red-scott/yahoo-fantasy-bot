643f024ffc8b573b672648ecd5e6b7d99b71ffd8
Red Scott <25727552+red-scott@users.noreply.github.com>
2019-10-07 09:56:52 -0700

    Added win probability to Score Update Message

--- bot/src/main/kotlin/bot/transformers/MatchUpDataTransformer.kt
+++ bot/src/main/kotlin/bot/transformers/MatchUpDataTransformer.kt
@@ -49,9 +49,9 @@ fun Observable<Pair<Team, Team>>.convertToScoreUpdateMessage(closeScoreUpdate: B
         }
     }.map {
         val message = "${it.first.name} vs. ${it.second.name}\\n" +
-                "${it.first.points}" +
+                "${it.first.points} (wp: ${it.first.winProbability}%)" +
                 " - " +
-                "${it.second.points}"
+                "${it.second.points} (wp: ${it.second.winProbability}%)"
 
         if (closeScoreUpdate) {
             Message.CloseScore(message)
